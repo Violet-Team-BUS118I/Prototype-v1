d6a428ffb817d6616be56d27d7af53ebd9d3ed0f
Giovanni Fernandez <giovanni@giovannifernandez.com>
2024-04-15 15:16:57 -0700

    Extra Edits

--- Prototype_Updated/app.py
+++ Prototype_Updated/app.py
@@ -1,3 +1,4 @@
+# Main App
 import streamlit as st
 from PROTOTYPES_AND_RESOURCES.PROTOTYPE_V2.SpeechAndChat import main as speech_and_chat_main
 from PROTOTYPES_AND_RESOURCES.PROTOTYPE_V2.EnergySaver import main as energy_saver_main

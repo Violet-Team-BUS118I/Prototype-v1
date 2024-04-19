a61631897dae79e35a1376ea46db2c3ce013c89c
Giovanni Fernandez <giovanni@giovannifernandez.com>
2024-04-18 19:16:31 -0700

    Organized Files

--- Prototypes/Prototype_Updated/app.py
+++ Prototypes/Prototype_Updated/app.py
@@ -4,9 +4,7 @@ from PROTOTYPES_AND_RESOURCES.PROTOTYPE_V3.OpenAITextToSpeech import main as tex
 from PROTOTYPES_AND_RESOURCES.PROTOTYPE_V3.SpeechAndChat import main as speech_and_chat_main
 from PROTOTYPES_AND_RESOURCES.PROTOTYPE_V3.FileSearch import main as file_and_search_main
 
-
-# Define your API key here
-API_KEY = "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"
+API_KEY = "YOUR-API-KEY-HERE"
 
 # Set up the page configuration
 st.set_page_config(page_title='Sustainability Suite', layout='wide')
